9721bd1f3793b90f0bad3ae5e02e784c2366421f
Jordi Flaquer Ferrer <flaquere@yahoo.es>
2021-03-03 11:20:13 +0100

    Init

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(() => {
+	var $scroller = $('.container');
+	$('a[href^="#"]').on('click',function (e) {
+    e.preventDefault();
+
+    var target = this.hash;
+    var $target = $(target);
+
+    $scroller.stop().animate({
+        'scrollTop': $target.offset().top - $scroller.offset().top + $scroller.scrollTop()
+    }, 900, 'swing', function () {
+        window.location.hash = target;
+    });
+  });
+})
+
+
+	
+  
